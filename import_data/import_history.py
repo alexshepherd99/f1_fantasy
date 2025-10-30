48f9c9c590b17097f8bedcbd627cdd830784db75
alexshepherd99 <alexshepherd99@gmail.com>
2025-10-30 15:01:17 +0000

    first pass input data sheet conversion

--- import_data/import_history.py
+++ import_data/import_history.py
@@ -2,3 +2,12 @@ import pandas as pd
 
 _FILE_ARCHIVE_INPUTS = "data/f1_fantasy_archive.xlsx"
 
+
+def convert_data_sheet(df_input: pd.DataFrame, season: int, id_cols: list, val_col: str) -> pd.DataFrame:
+    df_melted = df_input.melt(
+        id_vars=id_cols,
+        var_name="Race",
+        value_name=val_col,
+    )
+    df_melted["Season"] = season
+    return df_melted
--- import_data/tests/test_import_history.py
+++ import_data/tests/test_import_history.py
@@ -1,6 +1,43 @@
 import pytest
+from pandas.testing import assert_frame_equal
 import pandas as pd
 
+from import_data.import_history import convert_data_sheet
 
-def test_convert_data_sheet():
-    assert False
+
+def test_convert_data_sheet_two_column():
+    df_input = pd.DataFrame(
+        columns=["Team", "Driver", 1, 2, 3, 4],
+        data=[
+            ["Team A", "Driver 1", 10, 15, 20, 25],
+            ["Team B", "Driver 2", 12, 18, 22, 28],
+            ["Team B", "Driver 3", 25, 43, 11, 63],            
+        ],
+    )
+
+    df_expected = pd.DataFrame(
+        columns=["Season", "Race", "Team", "Driver", "Points"],
+        data=[
+            [2023, 1, "Team A", "Driver 1", 10],
+            [2023, 2, "Team A", "Driver 1", 15],
+            [2023, 3, "Team A", "Driver 1", 20],
+            [2023, 4, "Team A", "Driver 1", 25],
+            [2023, 1, "Team B", "Driver 2", 12],
+            [2023, 2, "Team B", "Driver 2", 18],
+            [2023, 3, "Team B", "Driver 2", 22],
+            [2023, 4, "Team B", "Driver 2", 28],
+            [2023, 1, "Team B", "Driver 3", 25],
+            [2023, 2, "Team B", "Driver 3", 43],
+            [2023, 3, "Team B", "Driver 3", 11],
+            [2023, 4, "Team B", "Driver 3", 63],
+        ]
+    )
+
+    df_actual = convert_data_sheet(
+        df_input=df_input,
+        season=2023,
+        id_cols=["Team", "Driver"],
+        val_col="Points",
+    )
+
+    assert_frame_equal(df_actual, df_expected)
