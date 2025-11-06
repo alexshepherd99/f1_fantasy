2a5d94c866bcf75093b60d369a62ef37dc0b96ff
alexshepherd99 <alexshepherd99@gmail.com>
2025-11-06 20:09:52 +0000

    Adding derivations

--- import_data/derivations.py
+++ import_data/derivations.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def derivation_cum_tot_points(race: int, ser_points: pd.Series) -> int:
-    # Cumulative Total Points up to (but not including) the current race
-    return ser_points[ser_points.index < race].sum().astype(int)
+    # Cumulative Total Points up to (and including) the current race
+    return ser_points[ser_points.index <= race].sum().astype(int)
