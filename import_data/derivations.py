76d224e655870acf7290d61ac1ca8e8df83b284f
alexshepherd99 <alexshepherd99@gmail.com>
2025-11-06 20:21:45 +0000

    Adding derivations

--- import_data/derivations.py
+++ import_data/derivations.py
@@ -4,3 +4,8 @@ import pandas as pd
 def derivation_cum_tot_points(race: int, ser_points: pd.Series) -> int:
     # Cumulative Total Points up to (and including) the current race
     return ser_points[ser_points.index <= race].sum().astype(int)
+
+
+def derivation_cum_tot_price(race: int, ser_price: pd.Series) -> float:
+    # Cumulative Total Price up to (and including) the current race
+    return ser_price[ser_price.index <= race].sum().astype(float)
--- tests/test_derivations.py
+++ tests/test_derivations.py
@@ -1,6 +1,10 @@
 import pandas as pd
+import pytest
 
-from import_data.derivations import derivation_cum_tot_points
+from import_data.derivations import (
+    derivation_cum_tot_points,
+    derivation_cum_tot_price,
+)
 
 
 def test_derivation_cum_tot_points():
@@ -20,3 +24,22 @@ def test_derivation_cum_tot_points():
     assert derivation_cum_tot_points(race=9, ser_points=ser_points) == 105
     assert derivation_cum_tot_points(race=10, ser_points=ser_points) == 115
     assert derivation_cum_tot_points(race=11, ser_points=ser_points) == 115
+
+
+def test_derivation_cum_tot_price():
+    ser_price = pd.Series(
+        [10.1, 15.2, 0, 25.3, 5.4, 0, 30.5, 20.6, None, 10.7],
+        index=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    # The expected points will include the current race from the cumulative total
+    assert derivation_cum_tot_price(race=1, ser_price=ser_price) == pytest.approx(10.1, abs=1e-2)
+    assert derivation_cum_tot_price(race=2, ser_price=ser_price) == pytest.approx(25.3, abs=1e-2)
+    assert derivation_cum_tot_price(race=3, ser_price=ser_price) == pytest.approx(25.3, abs=1e-2)
+    assert derivation_cum_tot_price(race=4, ser_price=ser_price) == pytest.approx(50.6, abs=1e-2)
+    assert derivation_cum_tot_price(race=5, ser_price=ser_price) == pytest.approx(56.0, abs=1e-2)
+    assert derivation_cum_tot_price(race=6, ser_price=ser_price) == pytest.approx(56.0, abs=1e-2)
+    assert derivation_cum_tot_price(race=7, ser_price=ser_price) == pytest.approx(86.5, abs=1e-2)
+    assert derivation_cum_tot_price(race=8, ser_price=ser_price) == pytest.approx(107.1, abs=1e-2)
+    assert derivation_cum_tot_price(race=9, ser_price=ser_price) == pytest.approx(107.1, abs=1e-2)
+    assert derivation_cum_tot_price(race=10, ser_price=ser_price) == pytest.approx(117.8, abs=1e-2)
+    assert derivation_cum_tot_price(race=11, ser_price=ser_price) == pytest.approx(117.8, abs=1e-2)
