1ea8c43576c7d84d4d504e26c8014707388e480e
alexshepherd99 <alexshepherd99@gmail.com>
2025-11-08 11:28:49 +0000

    Further work on derivations

--- import_data/derivations.py
+++ import_data/derivations.py
@@ -3,15 +3,18 @@ import numpy as np
 
 
 def derivation_cum_tot_driver(df_input: pd.DataFrame) -> pd.DataFrame:
-    #Ensure rows are ordered by the grouping keys and race
-    df = df_input.sort_values(["Season", "Constructor", "Driver", "Race"], ignore_index=True)
+    # Group up drivers, regardless of which constructor they drove for
+    df = df_input.groupby(["Season", "Driver", "Race"]).sum().reset_index()
+
+    # Ensure rows are ordered by the grouping keys and race
+    df = df.sort_values(["Season", "Driver", "Race"], ignore_index=True)
 
     # Cumulative totals per group, treating NaN as zero
-    df["Points Cumulative"] = df.groupby(["Season", "Constructor", "Driver"])["Points"] \
+    df["Points Cumulative"] = df.groupby(["Season", "Driver"])["Points"] \
                         .transform(lambda s: s.fillna(0).cumsum()) \
                         .astype(int)  # if you know result should be integer
 
-    df["Price Cumulative"] = df.groupby(["Season", "Constructor", "Driver"])["Price"] \
+    df["Price Cumulative"] = df.groupby(["Season", "Driver"])["Price"] \
                         .transform(lambda s: s.fillna(0.0).cumsum())  # float
 
     # Expected cumulative points-per-money (ppm) = cumulative points divided by cumulative price.
