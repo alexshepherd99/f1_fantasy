617b4233003048080b5284485ef3f73022684564
alexshepherd99 <alexshepherd99@gmail.com>
2025-11-11 21:37:30 +0000

    Use rolling ppm

--- scripts/check_run_ppm.py
+++ scripts/check_run_ppm.py
@@ -12,13 +12,10 @@ if __name__ == "__main__":
     setup_logging()
 
     df_all_archive_data_driver = load_all_archive_data(asset_type=AssetType.DRIVER)
-    df_derived_data_driver = derivation_cum_tot_driver(df_all_archive_data_driver)
+    df_derived_data_driver = derivation_cum_tot_driver(df_all_archive_data_driver, rolling_window=3)
 
     df_all_archive_data_constructor = load_all_archive_data(asset_type=AssetType.CONSTRUCTOR)
-    logging.info(f"Derived driver cumulative PPM data, shape: {df_all_archive_data_constructor.shape}")
-
-    df_derived_data_constructor = derivation_cum_tot_constructor(df_all_archive_data_constructor)
-    logging.info(f"Derived constructor cumulative PPM data, shape: {df_derived_data_constructor.shape}")
+    df_derived_data_constructor = derivation_cum_tot_constructor(df_all_archive_data_constructor, rolling_window=3)
 
     with pd.ExcelWriter(_FILE_DERIVED) as writer:
         df_derived_data_driver.to_excel(writer, sheet_name="Driver PPM Cumulative", index=False)
