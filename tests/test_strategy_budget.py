e8920b513fabd16cb846a6858079e267eacba274
alexshepherd99 <alexshepherd99@gmail.com>
2025-11-26 20:24:19 +0000

    Budget strategy tests

--- tests/test_strategy_budget.py
+++ tests/test_strategy_budget.py
@@ -46,3 +46,124 @@ def test_strat_budget_three_swaps(
     assert constructors["AST"].value() == 1.0
 
     assert problem.objective.value() == 44.5
+
+
+def test_strat_budget_limited_cost(
+    fixture_all_available_drivers,
+    fixture_all_available_constructors,
+    fixture_asset_prices,
+    fixture_pairings,
+):
+    strat = StrategyMaxBudget(
+        team_drivers=["VER", "LEC", "HAM", "ALO"],
+        team_constructors=["MCL", "FER"],
+        all_available_drivers=fixture_all_available_drivers,
+        all_available_constructors=fixture_all_available_constructors,
+        all_available_driver_pairs=fixture_pairings,
+        max_cost=40.0,
+        max_moves=3,
+        prices_assets=fixture_asset_prices,
+    )
+    problem = strat.execute("max_budget_limited_cost")
+
+    drivers = strat._lp_variables[VarType.TeamDrivers]
+    assert drivers["VER"].value() == 0.0
+    assert drivers["LEC"].value() == 0.0
+    assert drivers["HAM"].value() == 0.0
+    assert drivers["ALO"].value() == 1.0
+    assert drivers["HUL"].value() == 1.0
+    assert drivers["MAG"].value() == 0.0
+    assert drivers["BOT"].value() == 0.0
+    assert drivers["NOR"].value() == 0.0
+    assert drivers["PIA"].value() == 1.0
+    assert drivers["TSU"].value() == 1.0
+
+    constructors = strat._lp_variables[VarType.TeamConstructors]
+    assert constructors["MCL"].value() == 1.0
+    assert constructors["FER"].value() == 1.0
+    assert constructors["RED"].value() == 0.0
+    assert constructors["MER"].value() == 0.0
+    assert constructors["AST"].value() == 0.0
+
+    assert problem.objective.value() == 40.0
+
+
+def test_strat_budget_no_moves(
+    fixture_all_available_drivers,
+    fixture_all_available_constructors,
+    fixture_asset_prices,
+    fixture_pairings,
+):
+    strat = StrategyMaxBudget(
+        team_drivers=["VER", "LEC", "HAM", "ALO"],
+        team_constructors=["MCL", "FER"],
+        all_available_drivers=fixture_all_available_drivers,
+        all_available_constructors=fixture_all_available_constructors,
+        all_available_driver_pairs=fixture_pairings,
+        max_cost=100.0,
+        max_moves=0,
+        prices_assets=fixture_asset_prices,
+    )
+    problem = strat.execute("max_budget_no_moves")
+
+    drivers = strat._lp_variables[VarType.TeamDrivers]
+    assert drivers["VER"].value() == 1.0
+    assert drivers["LEC"].value() == 1.0
+    assert drivers["HAM"].value() == 1.0
+    assert drivers["ALO"].value() == 1.0
+    assert drivers["HUL"].value() == 0.0
+    assert drivers["MAG"].value() == 0.0
+    assert drivers["BOT"].value() == 0.0
+    assert drivers["NOR"].value() == 0.0
+    assert drivers["PIA"].value() == 0.0
+    assert drivers["TSU"].value() == 0.0
+
+    constructors = strat._lp_variables[VarType.TeamConstructors]
+    assert constructors["MCL"].value() == 1.0
+    assert constructors["FER"].value() == 1.0
+    assert constructors["RED"].value() == 0.0
+    assert constructors["MER"].value() == 0.0
+    assert constructors["AST"].value() == 0.0
+
+    assert problem.objective.value() == 20.0
+
+
+def test_strat_budget_driver_unavailable(
+    fixture_all_available_drivers,
+    fixture_all_available_constructors,
+    fixture_asset_prices,
+    fixture_pairings,
+):
+    strat = StrategyMaxBudget(
+        team_drivers=["RUS", "LEC", "HAM", "ALO"],  # RUS is unavailable
+        team_constructors=["MCL", "FER"],
+        all_available_drivers=fixture_all_available_drivers,
+        all_available_constructors=fixture_all_available_constructors,
+        all_available_driver_pairs=fixture_pairings,
+        max_cost=100.0,
+        max_moves=1,
+        prices_assets=fixture_asset_prices,
+    )
+    problem = strat.execute("max_budget_driver_unavailable")
+
+    drivers = strat._lp_variables[VarType.TeamDrivers]
+    assert drivers["RUS"].value() == 0.0
+    assert drivers["VER"].value() == 0.0
+    assert drivers["LEC"].value() == 1.0
+    assert drivers["HAM"].value() == 1.0
+    assert drivers["ALO"].value() == 1.0
+    assert drivers["HUL"].value() == 0.0
+    assert drivers["MAG"].value() == 0.0
+    assert drivers["BOT"].value() == 0.0
+    assert drivers["NOR"].value() == 0.0
+    assert drivers["PIA"].value() == 1.0
+    assert drivers["TSU"].value() == 0.0
+
+    constructors = strat._lp_variables[VarType.TeamConstructors]
+    assert constructors["MCL"].value() == 1.0
+    assert constructors["FER"].value() == 1.0
+    assert constructors["RED"].value() == 0.0
+    assert constructors["MER"].value() == 0.0
+    assert constructors["AST"].value() == 0.0
+
+    assert problem.objective.value() == 29.0
