c02f656cfedb58bb5444d4a751064f4d72d87a48
alexshepherd99 <alexshepherd99@gmail.com>
2025-11-26 20:14:09 +0000

    Strategy budget testing

--- tests/test_strategy_budget.py
+++ tests/test_strategy_budget.py
@@ -1,3 +1,4 @@
+from linear.strategy_base import VarType
 from tests.test_strategy_base import (
     fixture_all_available_drivers,
     fixture_all_available_constructors,
@@ -23,6 +24,25 @@ def test_strat_budget_three_swaps(
         max_moves=3,
         prices_assets=fixture_asset_prices,
     )
-    problem = strat.execute("Maximize Budget Strategy - 3 swaps")
-    assert problem.objective.value() == 100.0
-    
+    problem = strat.execute("max_budget_strat_3")
+
+    drivers = strat._lp_variables[VarType.TeamDrivers]
+    assert drivers["VER"].value() == 0.0
+    assert drivers["LEC"].value() == 0.0
+    assert drivers["HAM"].value() == 1.0
+    assert drivers["ALO"].value() == 1.0
+    assert drivers["HUL"].value() == 0.0
+    assert drivers["MAG"].value() == 0.0
+    assert drivers["BOT"].value() == 0.0
+    assert drivers["NOR"].value() == 0.0
+    assert drivers["PIA"].value() == 1.0
+    assert drivers["TSU"].value() == 1.0
+
+    constructors = strat._lp_variables[VarType.TeamConstructors]
+    assert constructors["MCL"].value() == 0.0
+    assert constructors["FER"].value() == 1.0
+    assert constructors["RED"].value() == 0.0
+    assert constructors["MER"].value() == 0.0
+    assert constructors["AST"].value() == 1.0
+
+    assert problem.objective.value() == 44.5
