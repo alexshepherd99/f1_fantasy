1c7b9a47f32084a17b0fa89f4a15324a5b3337d2
alexshepherd99 <alexshepherd99@gmail.com>
2025-12-11 07:14:14 +0000

    P2PM strategy

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import functools
 
 from common import AssetType
@@ -19,3 +20,29 @@ def load_with_derivations(season: int) -> tuple[pd.DataFrame, pd.DataFrame, pd.D
     df_constructor_ppm = derivation_cum_tot_constructor(df_constructor, rolling_window=3)
 
     return (df_driver_ppm, df_constructor_ppm, df_driver_pairs)
+
+
+def safe_to_float(value, default=0.0) -> float:
+    """
+    Safely converts a value (like a string or int) to a float.
+
+    Args:
+        value: The input value to convert.
+        default: The value to return if conversion fails (default is 0.0).
+
+    Returns:
+        The float representation of the value, or the default value on failure.
+    """
+    if value is None:
+        return default
+    elif value is np.nan:
+        return default
+    
+    try:
+        # The built-in float() function handles standard formats,
+        # including leading/trailing whitespace and scientific notation.
+        return float(value)
+    except (ValueError, TypeError):
+        # Catch errors if the string is not a valid number, e.g., "hello"
+        # or if the input type is fundamentally incompatible.
+        return default
--- linear/strategy_p2pm.py
+++ linear/strategy_p2pm.py
@@ -1,5 +1,7 @@
-from pulp import LpProblem, LpMaximize
+from pulp import LpProblem, LpMaximize, lpSum
 
+from helpers import safe_to_float
+from import_data.derivations import DerivationType, get_derivation_name
 from linear.strategy_base import StrategyBase, VarType
 
 
@@ -10,16 +12,24 @@ class StrategyMaxP2PM(StrategyBase):
     def get_problem(self) -> LpProblem:
         problem = LpProblem(self.__class__.__name__, LpMaximize)
 
+        deriv_name = get_derivation_name(DerivationType.P2PM_CUMULATIVE, 3)
+
         # Ensure anything without the P2PM value defaults to zero, i.e. it's worth nothing
+        for d in self._all_available_drivers + self._all_available_constructors:
+            if self._derivs_assets[deriv_name].get(d) is None:
+                self._derivs_assets[deriv_name][d] = 0.0
+            else:
+                self._derivs_assets[deriv_name][d] = safe_to_float(self._derivs_assets[deriv_name][d])
 
-        # Costs as based on the team selection in the above LP variables
-        #cost_drivers = [self._prices_assets[i] * self._lp_variables[VarType.TeamDrivers][i] for i in driver_list]
-        #cost_constructors = [self._prices_assets[i] * self._lp_variables[VarType.TeamConstructors][i] for i in constructor_list]
+        print(self._derivs_assets[deriv_name])
 
-        # Variable and constraint for total cost
-        #self._lp_variables[VarType.TotalCost] = lpSum(cost_drivers + cost_constructors)
+        # P2PM values as based on the team selection, using the LP variables already provided by the base class
+        p2pm_drivers = [self._derivs_assets[deriv_name][i] * self._lp_variables[VarType.TeamDrivers][i] for i in self._all_available_drivers]
+        p2pm_constructors = [self._derivs_assets[deriv_name][i] * self._lp_variables[VarType.TeamConstructors][i] for i in self._all_available_constructors]
+
+        # Variable for total P2PM
+        self._lp_variables[VarType.OptimiseMax] = lpSum(p2pm_drivers + p2pm_constructors)
 
         # Optimise for this
-        #OptimiseMax
-
+        problem += self._lp_variables[VarType.OptimiseMax]
         return problem
