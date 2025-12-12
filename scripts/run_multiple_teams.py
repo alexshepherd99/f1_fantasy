2a06e73a81315dd31d9de0ef9714dea4c0c37f9e
alexshepherd99 <alexshepherd99@gmail.com>
2025-12-12 17:58:10 +0000

    Finish back-testing data generation

--- scripts/run_multiple_teams.py
+++ scripts/run_multiple_teams.py
@@ -2,18 +2,21 @@ import os
 import pandas as pd
 import logging
 
-from common import setup_logging
+from common import F1_SEASON_CONSTRUCTORS, setup_logging
 from helpers import load_with_derivations
+from linear.strategy_base import StrategyBase
 from linear.strategy_budget import StrategyMaxBudget
+from linear.strategy_p2pm import StrategyMaxP2PM
+from linear.strategy_zero_stop import StrategyZeroStop
 from races.first_picks import get_starting_combinations
 from races.season import factory_race, factory_season
 from races.team import Team, factory_team_row
 from scripts.run_single_team import run_for_team
 
-_SEASON = 2025
+_SEASONS = F1_SEASON_CONSTRUCTORS.keys()
 _FILE_BATCH_RESULTS_PARQET = "outputs/f1_fantasy_results_batch.parquet"
 _FILE_BATCH_RESULTS_EXCEL = "outputs/f1_fantasy_results_batch.xlsx"
-_STRATEGY = StrategyMaxBudget
+_STRATEGIES = [StrategyMaxBudget, StrategyZeroStop, StrategyMaxP2PM]
 
 
 def get_starting_key(strat_name: str, season: int, team: Team) -> str:
@@ -27,19 +30,20 @@ def open_batch_results_file(fn: str) -> pd.DataFrame:
     return pd.read_parquet(fn)
 
 
-if __name__ == "__main__":
-    setup_logging()
+def write_batch_results(df_batch_results: pd.DataFrame, rows_append: list):
+    df_tmp = pd.DataFrame(rows_append)
+    df_batch_results = pd.concat([df_batch_results, df_tmp])
+    df_batch_results.to_parquet(_FILE_BATCH_RESULTS_PARQET)
 
-    # TODO update final race points in sheet
-    # TODO PP2M strategy
 
-    (df_driver_ppm, df_constructor_ppm, df_driver_pairs) = load_with_derivations(season=_SEASON)
+def run_strategy_for_season(season_year: int, strategy: type[StrategyBase]):
+    (df_driver_ppm, df_constructor_ppm, df_driver_pairs) = load_with_derivations(season=season_year)
     
     _season = factory_season(
         df_driver_ppm,
         df_constructor_ppm,
         df_driver_pairs,
-        _SEASON,
+        season_year,
     )
     
     _race_first = factory_race(
@@ -50,7 +54,7 @@ if __name__ == "__main__":
     )
 
     _df_batch_results = open_batch_results_file(_FILE_BATCH_RESULTS_PARQET)
-    _df_combinations = get_starting_combinations(_SEASON, 1, 99.5)
+    _df_combinations = get_starting_combinations(season_year, 1, 99.5)
 
     counter = 0
     skipped = 0
@@ -58,14 +62,14 @@ if __name__ == "__main__":
 
     for _idx, _row in _df_combinations.iterrows():
         _team = factory_team_row(_row.to_dict(), _race_first)
-        _sim_key = get_starting_key(_STRATEGY.__name__, _SEASON, _team)
+        _sim_key = get_starting_key(strategy.__name__, season_year, _team)
 
         if _sim_key in _df_batch_results["sim_key"].unique():
             logging.info(f"Skipping batch for {_sim_key}")
             skipped += 1
 
         else:
-            _rows_intermediate = run_for_team(_STRATEGY, _team, _season, _SEASON, 1)
+            _rows_intermediate = run_for_team(strategy, _team, _season, season_year, 1)
             _row_final = _rows_intermediate[-1]
             _row_final["sim_key"] = _sim_key
             _rows_append.append(_row_final)
@@ -73,7 +77,18 @@ if __name__ == "__main__":
             counter += 1
             if counter % 100 == 0:
                 logging.info(f"Batch {counter} of {len(_df_combinations.index)-skipped}, writing to disk...")
-                _df_tmp = pd.DataFrame(_rows_append)
-                _df_batch_results = pd.concat([_df_batch_results, _df_tmp])
-                _df_batch_results.to_parquet(_FILE_BATCH_RESULTS_PARQET)
+                write_batch_results(_df_batch_results, _rows_append)
                 _rows_append = []
+
+    # Write any remaining results
+    logging.info(f"Writing remaining {len(_rows_append)} batches to disk...")
+    write_batch_results(_df_batch_results, _rows_append)
+    _rows_append = []
+
+
+if __name__ == "__main__":
+    setup_logging()
+
+    for _season_year in _SEASONS:
+        for _strategy in _STRATEGIES:
+            run_strategy_for_season(_season_year, _strategy)
