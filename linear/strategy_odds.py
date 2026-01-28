f23bfba218e4f436f2ada2d275313f8bd1dea089
Alex Shepherd <alexshepherd99@gmail.com>
2026-01-28 18:43:42 +0000

    Further work on betting odds strategy

--- linear/strategy_odds.py
+++ linear/strategy_odds.py
@@ -1,14 +1,17 @@
 import pandas as pd
 import functools
+from pulp import LpProblem, LpMaximize, lpSum
 
 from common import AssetType
+from linear.strategy_base import StrategyBase
 
-_FILE_BETTING_ODDS = "data/f1_betting_odds.csv"
+
+_FILE_BETTING_ODDS = "data/f1_betting_odds.xslx"
 
 
 def odds_to_pct(odds: str) -> float:
-    if odds is None:
-        raise ValueError(f"odds_to_pct invalid input {odds}")
+    if (odds is None) or (odds == ""):
+        return 0.0
 
     odds = odds.replace(":", "/")
     odds = odds.replace("-", "/")
@@ -35,7 +38,7 @@ def odds_to_pct(odds: str) -> float:
 @functools.cache
 def load_odds(ass_typ: AssetType, season_year: int, race_num: int, fn: str=_FILE_BETTING_ODDS) -> dict[str, float]:
     # Load and filter
-    df_all = pd.read_csv(fn)
+    df_all = pd.read_excel(fn)
     df_all = df_all[df_all["Season"] == season_year]
     df_all = df_all[df_all["Race"] == race_num]
 
@@ -53,3 +56,18 @@ def load_odds(ass_typ: AssetType, season_year: int, race_num: int, fn: str=_FILE
     df_all = df_all[[ass_typ.value, "Odds"]]
     df_all = df_all.set_index(ass_typ.value)
     return df_all["Odds"].to_dict()
+
+
+class StrategyBettingOdds(StrategyBase):
+    """Strategy that maximises selection based on betting odds"""
+    def __init__(self, *args, fn_odds: str=_FILE_BETTING_ODDS, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        odds_assets_drv = load_odds(AssetType.DRIVER, self._season_year, self._race_num, fn=fn_odds)
+        odds_assets_con = load_odds(AssetType.CONSTRUCTOR, self._season_year, self._race_num, fn=fn_odds)
+
+        self._odds_assets = odds_assets_drv | odds_assets_con
+
+    def get_problem(self) -> LpProblem:
+        return None
+
--- tests/test_strategy_odds.py
+++ tests/test_strategy_odds.py
@@ -1,9 +1,9 @@
 import pytest
 
 from common import AssetType
-from linear.strategy_odds import load_odds, odds_to_pct
+from linear.strategy_odds import StrategyBettingOdds, load_odds, odds_to_pct
 
-_TEST_ODDS_FILE = "data/test_betting_odds.csv"
+_TEST_ODDS_FILE = "data/test_betting_odds.xlsx"
 
 
 def test_odds_to_pct():
@@ -12,11 +12,9 @@ def test_odds_to_pct():
     assert odds_to_pct("100-1") == pytest.approx(0.01, abs=0.001)
     assert odds_to_pct("10/2") == pytest.approx(0.2, abs=0.001)
     assert odds_to_pct("9/4") == pytest.approx(0.4444, abs=0.0001)
+    assert odds_to_pct("") == 0.0
+    assert odds_to_pct(None) == 0.0
 
-    with pytest.raises(ValueError):
-        odds_to_pct(None)
-    with pytest.raises(ValueError):
-        odds_to_pct("")
     with pytest.raises(ValueError):
         odds_to_pct("0/100")
     with pytest.raises(ValueError):
@@ -63,3 +61,45 @@ def test_load_odds():
     }
     for k in dict_drv_exp.keys():
         assert pytest.approx(dict_drv[k], 0.001) == dict_drv_exp[k]
+
+
+def test_strategy_odds_load_data():
+    # Basic check to ensure that odds data file is loaded, just check size of data set and one of the keys
+    strat = StrategyBettingOdds(
+        fn_odds=_TEST_ODDS_FILE,
+        team_drivers=[],
+        team_constructors=[],
+        all_available_drivers=[],
+        all_available_constructors=[],
+        all_available_driver_pairs={},
+        prev_available_driver_pairs={},
+        max_cost=0.0,
+        max_moves=0,
+        prices_assets={},
+        derivs_assets={},
+        race_num=1,
+        season_year=1900,
+    )
+    assert len(strat._odds_assets) == 6
+    assert strat._odds_assets["DRV_test_A@CON_test_1"] == 0.01
+    assert strat._odds_assets["CON_test_1"] == 0.1
+
+
+def test_strategy_odds_missing_assets():
+    # Add extra constructor and extra driver which isn't in odds data, check that odds for these is set to 0.0
+    assert False
+
+
+def test_strategy_odds_check_assets():
+    # Check that driver odds are directly taken from file, and constructor odds are doubled to reflect extra value
+    assert False
+
+
+def test_strategy_odds_run():
+    # Create a sub-optimal team with many possible moves, check best two moves are applied
+    assert False
+
+
+def test_strategy_odds_select_drs():
+    # DRS selection should pick the driver with the best odds, not the highest value
+    assert False
