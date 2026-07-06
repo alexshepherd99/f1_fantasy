0d6c97da24b1124733129301ef9995e6bf816490
Alex Shepherd <alexshepherd99@gmail.com>
2026-07-06 19:08:35 +0100

    Move functions to load betting odds out of strategy into import_data

--- linear/strategy_odds.py
+++ linear/strategy_odds.py
@@ -1,63 +1,8 @@
-import pandas as pd
-import functools
 from pulp import LpProblem, LpMaximize, lpSum, LpVariable
 
 from common import AssetType
 from linear.strategy_base import StrategyBase, VarType
-
-
-_FILE_BETTING_ODDS = "data/f1_betting_odds.xlsx"
-
-
-def odds_to_pct(odds: str) -> float:
-    if (odds is None) or (odds == ""):
-        return 0.0
-
-    odds = odds.replace(":", "/")
-    odds = odds.replace("-", "/")
-    
-    if odds.count("/") != 1:
-        raise ValueError(f"odds_to_pct invalid input {odds}")
-
-    odds_values = odds.split("/")
-    if len(odds_values) != 2:
-        raise ValueError(f"odds_to_pct invalid input {odds}")
-    
-    odds_left = int(odds_values[0])
-    odds_right = int(odds_values[1])
-
-    if odds_right > odds_left:
-        raise ValueError(f"odds_to_pct invalid input {odds}")
-    
-    if (odds_right == 0) or (odds_left == 0):
-        raise ValueError(f"odds_to_pct invalid input {odds}")
-
-    return 1 / (odds_left / odds_right)
-
-
-@functools.cache
-def load_odds(ass_typ: AssetType, season_year: int, race_num: int, fn: str=_FILE_BETTING_ODDS) -> dict[str, float]:
-    # Load and filter
-    df_all = pd.read_excel(fn)
-    df_all = df_all[df_all["Season"] == season_year]
-    df_all = df_all[df_all["Race"] == race_num]
-
-    # Process Odds column
-    df_all = df_all[["Driver", "Constructor", "Season", "Race", "Odds"]]
-    df_all["Odds"] = df_all["Odds"].apply(odds_to_pct)
-
-    # Select asset type
-    if ass_typ == AssetType.CONSTRUCTOR:
-        # Constructor odds in this case is actually constructor value, so it the sum of the driver odds (value)
-        df_all = df_all.groupby("Constructor").sum().reset_index()
-    elif ass_typ == AssetType.DRIVER:
-        df_all = df_all[~df_all["Driver"].isna()]
-        df_all["Driver"] = df_all["Driver"].astype(str) + "@" + df_all["Constructor"].astype(str)
-
-    # Convert to dictionary
-    df_all = df_all[[ass_typ.value, "Odds"]]
-    df_all = df_all.set_index(ass_typ.value)
-    return df_all["Odds"].to_dict()
+from import_data.odds import load_odds, _FILE_BETTING_ODDS
 
 
 class StrategyBettingOdds(StrategyBase):
---
+++ tests/test_odds.py
@@ -0,0 +1,64 @@
+import pytest
+
+from common import AssetType
+from import_data.odds import load_odds, odds_to_pct
+
+_TEST_ODDS_FILE = "data/test_betting_odds.xlsx"
+
+
+def test_odds_to_pct():
+    assert odds_to_pct("100/1") == pytest.approx(0.01, abs=0.001)
+    assert odds_to_pct("100:1") == pytest.approx(0.01, abs=0.001)
+    assert odds_to_pct("100-1") == pytest.approx(0.01, abs=0.001)
+    assert odds_to_pct("10/2") == pytest.approx(0.2, abs=0.001)
+    assert odds_to_pct("9/4") == pytest.approx(0.4444, abs=0.0001)
+    assert odds_to_pct("") == 0.0
+    assert odds_to_pct(None) == 0.0
+
+    with pytest.raises(ValueError):
+        odds_to_pct("0/100")
+    with pytest.raises(ValueError):
+        odds_to_pct("100/0")
+    with pytest.raises(ValueError):
+        odds_to_pct("1/100")
+    with pytest.raises(ValueError):
+        odds_to_pct("100")
+    with pytest.raises(ValueError):
+        odds_to_pct("/")
+    with pytest.raises(ValueError):
+        odds_to_pct("/1")
+    with pytest.raises(ValueError):
+        odds_to_pct("100/")
+    with pytest.raises(ValueError):
+        odds_to_pct("100/100/100")
+    with pytest.raises(ValueError):
+        odds_to_pct("string")
+
+
+def test_load_odds():
+    assert not load_odds(ass_typ=AssetType.CONSTRUCTOR, season_year=9999, race_num=1, fn=_TEST_ODDS_FILE)
+    assert not load_odds(ass_typ=AssetType.CONSTRUCTOR, season_year=1900, race_num=99, fn=_TEST_ODDS_FILE)
+    assert not load_odds(ass_typ=AssetType.DRIVER, season_year=9999, race_num=1, fn=_TEST_ODDS_FILE)
+    assert not load_odds(ass_typ=AssetType.DRIVER, season_year=1900, race_num=99, fn=_TEST_ODDS_FILE)
+
+    dict_drv = load_odds(ass_typ=AssetType.DRIVER, season_year=1900, race_num=1, fn=_TEST_ODDS_FILE)
+    assert len(dict_drv) == 4
+
+    dict_drv_exp = {
+        "DRV_test_A@CON_test_1": 0.01,
+        "DRV_test_B@CON_test_1": 0.04,
+        "DRV_test_C@CON_test_2": 0.222,
+        "DRV_test_D@CON_test_2": 0.5,
+    }
+    for k in dict_drv_exp.keys():
+        assert pytest.approx(dict_drv[k], 0.001) == dict_drv_exp[k]
+
+    dict_con = load_odds(ass_typ=AssetType.CONSTRUCTOR, season_year=1900, race_num=1, fn=_TEST_ODDS_FILE)
+    assert len(dict_con) == 2
+
+    dict_con_exp = {
+        "CON_test_1": 0.05,
+        "CON_test_2": 0.722,
+    }
+    for k in dict_con_exp.keys():
+        assert pytest.approx(dict_con[k], 0.001) == dict_con_exp[k]
